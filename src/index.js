238727bd027076bb7027e10675d7e84294dab75e
yuvanis <75557443+yuvanis@users.noreply.github.com>
2021-07-01 22:58:20 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) {
+    return false;
+  }
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let bracket = bracketsConfig[i].join('');
+    if (str.includes(bracket)) {
+      str = str.replace(bracket, '');
+      i = -1;
+    }
+  }
+  if (str == '') {
+    return true;
+  } else {
+    return false;
+  }
 }
